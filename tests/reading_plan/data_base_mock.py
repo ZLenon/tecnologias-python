82d2b6a9eeb504908bb0dd53f614093a3ed6bf5b
Lenon Nascimento <lenoninformatica@gmail.com>
2023-09-13 11:21:26 -0300

    Testes: função de noticas novas

---
+++ tests/reading_plan/data_base_mock.py
@@ -0,0 +1,40 @@
+# Você pode adicionar ou alterar mais dicionários à lista conforme necessário.
+
+mock_db = [
+    {
+        "url": "https://exemplo.com/post1",
+        "title": "Título do Post 1",
+        "timestamp": "01/01/2001",
+        "writer": "Autor 1",
+        "reading_time": 2,
+        "summary": "Resumo do Post 1",
+        "category": "Tecnologia",
+    },
+    {
+        "url": "https://exemplo.com/post2",
+        "title": "Título do Post 2",
+        "timestamp": "02/02/2002",
+        "writer": "Autor 2",
+        "reading_time": 10,
+        "summary": "Resumo do Post 2",
+        "category": "Ciência",
+    },
+    {
+        "url": "https://exemplo.com/post3",
+        "title": "Título do Post 3",
+        "timestamp": "03/03/2003",
+        "writer": "Autor 3",
+        "reading_time": 1,
+        "summary": "Resumo do Post 3",
+        "category": "Entretenimento",
+    },
+    {
+        "url": "https://exemplo.com/post4",
+        "title": "Título do Post 4",
+        "timestamp": "03/03/2003",
+        "writer": "Autor 4",
+        "reading_time": 2,
+        "summary": "Resumo do Post 4",
+        "category": "Games",
+    },
+]
--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,18 @@
-from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from tech_news.analyzer.reading_plan import (
+    ReadingPlanService,
+)  # noqa: F401, E261, E501
+import pytest
+from unittest.mock import patch
+from tests.reading_plan.data_base_mock import mock_db
 
 
-def test_reading_plan_group_news():
-    pass
+# Req 6
+@patch("tech_news.analyzer.reading_plan.find_news")
+def test_reading_plan_group_news(db):
+    db.return_value = mock_db
+    x = ReadingPlanService.group_news_for_available_time(2)
+    assert len(x["readable"]) == 3
+    assert len(x["unreadable"]) == 1
+    assert x["readable"][0]["unfilled_time"] == 0
+    with pytest.raises(ValueError):
+        ReadingPlanService.group_news_for_available_time(0)
