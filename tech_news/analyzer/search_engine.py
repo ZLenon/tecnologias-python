b4ff7c42ad7d5ca15b02110917b10193b4d2e11a
Lenon Nascimento <lenoninformatica@gmail.com>
2023-09-13 10:08:45 -0300

    Feat: Função de busca por data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
-from tech_news.database import find_news
+from tech_news.database import find_news, db
+from datetime import datetime
 
 
 # Req 7
@@ -14,8 +15,16 @@ def search_by_title(title):
 
 # Req 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        hora = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+
+        horario = {"timestamp": hora}
+        infos = {"_id": 99, "title": 99, "url": 99}
+        r = db.news.find(horario, infos)
+
+        return [(news["title"], news["url"]) for news in r]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Req 9
