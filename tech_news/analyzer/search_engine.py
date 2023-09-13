31eafdf137b9b75c9df937d76c8d942dbbeaecd2
Lenon Nascimento <lenoninformatica@gmail.com>
2023-09-13 09:23:11 -0300

    Feat: funçao de busca por titulo

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,16 +1,24 @@
-# Requisito 7
+from tech_news.database import find_news
+
+
+# Req 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    print(title)
+    news = find_news()
+    list_news = list()
+    for new in news:
+        if title.lower() in new["title"].lower():
+            list_news.append((new["title"], new["url"]))
+    return list_news
 
 
-# Requisito 8
+# Req 8
 def search_by_date(date):
     """Seu código deve vir aqui"""
     raise NotImplementedError
 
 
-# Requisito 9
+# Req 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
     raise NotImplementedError
